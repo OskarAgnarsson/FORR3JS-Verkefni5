da9a37d1c6ed138c2fda00b205d94594af9080b6
OskarAgnarsson <oskarag309@nemi.tskoli.is>
2019-10-22 14:47:54 +0000

    Gerði nokkrar spurningar

--- docs/verk51.js
+++ docs/verk51.js
@@ -2,18 +2,37 @@ const spurningar = [
     "Afhverju er getElementById() fljótleglegasta aðferðin?",
     "Hvað er málið með auða hnúta (e. whitespace nodes) og DOM tréið?",
     "Hvað er static og live NodeList, hver er munurinn á NodeList og HTMLCollection?",
-    "Hvað er event í eftirfarandi kóða og hvað er gert með því?",
+    `Hvað er event í eftirfarandi kóða og hvað er gert með því? <br><pre><code>
+    &lta href='https://developer.mozilla.org/'>MDN&lt/a>
+    &ltscript>
+        let link = document.querySelector("a");
+        link.addEventListener("click", event => {
+            console.log('Nope.');
+            event.preventDefault();
+        });
+    &lt/script></code></pre>`,
     "Af þremur leiðum til að binda event þá er AddEventListener() nýjust en afhverju er hún betri en hinar?",
-    "Hver er munurinn á true og false í AddEventListener?",
-    "this vísar í Event listener á html element en ekki á object. Þú getur notað bind() til að breyta því, leystu eftirfarandi kóðadæmi með notkun á bind() til að birta í console “My name is Sam“ en ekki undefined."
+    `Hver er munurinn á true og false í AddEventListener? <br><pre><code>
+    elem.addEventListener("click", handlerFunction, true);
+    elem.addEventListener("click", handlerFunction, false);
+    </code></pre>`,
+    `this vísar í Event listener á html element en ekki á object. Þú getur notað bind() til að breyta því, leystu eftirfarandi kóðadæmi með notkun á bind() til að birta í console “My name is Sam“ en ekki undefined.<br><pre><code>
+    let Person = {
+        name: 'Sam',
+        sayName: function(){
+        console.log('My name is '+ this.name);
+        }
+       };
+    buttonEl.addEventListener('click', Person.sayName);
+    </code></pre>`
 ];
 
 const svor = [
     "Því getElementById() fer í gegnum tréð og stoppar strax og það finnur rétta id.",
-    "Þegar þú setur inn whitespace í HTML getur það búið til node(hnút) sem er tómur",
-    "NodeList er hlutur sem er samansöfnun af nodes(ekki Array), static NodeList er bara listinn þegar þú sækir hann, live NodeList uppfærist sjálfkrafa.\nMunurinn á NodeList og HTMLCollection er sá að NodeList heldur utan um alla node en HTMLCollections halda bara utan um elementa og HTMLCollection er alltaf live en NodeList getur verið static og live.",
-    "",
-    "",
+    "Þegar þú setur inn whitespace í HTML getur það búið til node(hnút) sem er tómur.",
+    "NodeList er hlutur sem er samansöfnun af nodes(ekki Array), static NodeList er bara listinn þegar þú sækir hann, live NodeList uppfærist sjálfkrafa.<br> Munurinn á NodeList og HTMLCollection er sá að NodeList heldur utan um alla node en HTMLCollections halda bara utan um elementa og HTMLCollection er alltaf live en NodeList getur verið static og live.",
+    "Ef maður clickar á linkinn console.loggar það 'Nope' en fer ekki á síðuna.",
+    "AddEventListener() leyfir þér að setja eins marga event handlera og þú vilt á element, það stöðvar conflict á milli falla fyrir sama event",
     "",
     ""
 ];
@@ -26,14 +45,15 @@ const titill = document.createElement("h2");
 titill.textContent = "Verkefni 5.1";
 verkefni_5_1.append(titill);
 const svaraListi = document.createElement("ol");
+
 for (let i = 0; i < spurningar.length; i++) {
     let q = spurningar[i];
     let a = svor[i];
     let svar = document.createElement("li");
     let spParagraph = document.createElement("p");
     let svParagraph = document.createElement("p");
-    spParagraph.textContent = q;
-    svParagraph.textContent = a;
+    spParagraph.innerHTML = q;
+    svParagraph.innerHTML = a;
     svar.append(spParagraph);
     svar.append(svParagraph);
     svaraListi.append(svar);
